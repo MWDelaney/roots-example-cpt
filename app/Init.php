0a5a2a243d4b7502a718a8d112e3ea5c2bd525b7
Michael W. Delaney <michael.delaney@gmail.com>
2017-07-07 20:00:14 -0400

    Update Init.php

--- app/Init.php
+++ app/Init.php
@@ -7,20 +7,9 @@ class Init {
 
 	function __construct() {
 
-    /**
-	   * Includes
-	   */
-	 		$includes = [
-	 		  'app/types/type-example.php',   // Post Type
-	 		];
-
-      // Require each file in the $includes array
-	 		foreach ($includes as $file) {
-	 		  require_once EXAMPLECPT_PLUGIN_DIR . $file;
-	 		}
-
-      // Unset $file for best practices.
-	 		unset($file);
+		// Include all post types
+		foreach (glob(EXAMPLECPT_PLUGIN_DIR . "app/types/*.php") as $filename) {
+			include $filename;
+		}
 	}
-
 }
