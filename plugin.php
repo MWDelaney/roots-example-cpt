1d12296a92d57386bf2eadd6544d3af79f3c3566
Michael W. Delaney <michael.delaney@gmail.com>
2017-07-07 20:19:51 -0400

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -8,14 +8,16 @@ Author: Michael W. Delaney
 Author URI:
 License: MIT
 */
+
 namespace MWD\ExampleCPT;
-/**
- * Set up autoloader
- */
+
+
+//Set up autoloader
 require __DIR__ . '/vendor/autoload.php';
-/**
-* Define Constants
-*/
+
+//Define Constants
 define( 'EXAMPLECPT_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'EXAMPLECPT_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+
+// Autoload the Init class
 $example_init = new Init();
